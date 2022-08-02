3dca581be44f8c80b5fe31923691fecfb79228a5
Arnoldo Gonzalez <arnoldogonzalezroman@gmail.com>
2022-08-02 09:52:20 -0400

    Make responsive the loading skeleton of characters and increase the line height of titles of characters

--- src/public/js/insert_characters.js
+++ src/public/js/insert_characters.js
@@ -11,7 +11,7 @@ export const insertCharacters = characters => {
         characterArticle.classList.add("flex", "flex-col", "items-center", "res:grid", "res:grid-cols-2", "res:grid-rows-desk-place", "bg-sky-200", "dark:bg-blue-700", "transition-colors", "duration-300", "border-2", "border-transparent", "hover:border-blue-700", "dark:hover:border-sky-200", "min-h-res-section", "res:min-h-0", "h-section-desk", "w-full", "max-w-lg", "res:max-w-full", "rounded")
 
         characterArticle.innerHTML = `
-        <h4 class="order-1 res:order-none font-bold text-center self-center justify-self-center text-blue-700 dark:text-sky-200 px-5 text-2xl res:text-3xl row-span-1 col-start-1 col-end-2">${name}</h4>
+        <h4 class="order-1 res:order-none font-bold text-center leading-loose self-center justify-self-center text-blue-700 dark:text-sky-200 px-5 text-2xl res:text-3xl row-span-1 col-start-1 col-end-2">${name}</h4>
         <p class="order-3 res:order-none self-center row-start-3 row-end-4 res:row-span-2 col-start-1 col-end-2 flex flex-col gap-y-3 res:h-4/5 px-5 py-4">
             <span class="block dark:text-white text-xl">Species: <b class="font-bold text-blue-700 dark:text-sky-200">${species}</b></span>
             <span class="block dark:text-white text-lg">Status: <b class="font-bold text-blue-700 dark:text-sky-200">${status}</b></span>
@@ -47,7 +47,7 @@ export const getCharacters = () => {
                 episodes: character.episode.length
             }
         })
-        if(!loadingCharacters.classList.contains("hidden")) loadingCharacters.classList.add("hidden")
+        if(!loadingCharacters.classList.contains("hidden")) loadingCharacters.classList.add("hidden", "res:hidden")
         if(!loadingAddCharacters.classList.contains("hidden")) loadingAddCharacters.classList.add("hidden")
         btnAddCharacters.classList.remove("hidden")
         insertCharacters(data)
